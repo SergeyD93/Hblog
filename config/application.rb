860fe6c199a35baf2857878bc36dfcd6e28600aa
SergeyD93 <seriyyy@gmail.com>
2016-04-14 18:50:45 -0400

    precompile js

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Hblog
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.compress = true
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w( application.js )
+    config.assets.precompile += %w( comment.js.erb )
   end
 end
